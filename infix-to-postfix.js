ea2c0fee971ce5b1afad4ed63f286f2de9d090bf
Aldrin Jenson <aldrinjenson@gmail.com>
2020-10-31 22:19:20 +0530

    Add infix-to-postfix converter

---
+++ infix-to-postfix.js
@@ -0,0 +1,57 @@
+// Program to Convert an infix expression to its postfix form using Stack Data Structure
+// use $ for unary + or unary -
+
+let infixExp = "A+(B+C*D+E)+F/G";
+let stack = [];
+let postFix = "";
+
+Array.prototype.peek = () => {
+  if (stack.length) return stack[stack.length - 1];
+  return 0;
+};
+
+const isOperand = (x) => {
+  return x.match(/[A-Z,a-z]/);
+};
+
+const value = {
+  $: 4, // for unary operators
+  "^": 3,
+  "*": 2,
+  "/": 2,
+  "+": 1,
+  "-": 1,
+  "(": 0,
+};
+
+const precedence = (a, b) => {
+  if (b === 0) return true; // stack empty condition
+  return value[a] > value[b];
+};
+
+for (const el of infixExp) {
+  // console.log(stack);  // uncomment to see state of stack
+  // console.log(postFix); // and postfix expression during intermediate stages
+  if (isOperand(el)) postFix += el;
+  else {
+    if (precedence(el, stack.peek())) stack.push(el);
+    else if (el === "(") stack.push(el);
+    else if (el === ")") {
+      while (stack.peek() !== "(") {
+        postFix += stack.pop();
+      }
+      stack.pop();
+    } else {
+      while (!precedence(el, stack.peek())) {
+        postFix += stack.pop();
+      }
+      stack.push(el);
+    }
+  }
+}
+
+while (stack.length) {
+  postFix += stack.pop();
+}
+
+console.log(postFix);
